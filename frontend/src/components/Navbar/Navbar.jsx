3bd1ec6f358a67ed710bc6a234bd3cad2a488f3f
Thibaud Dupuis <124290694+ThibaudDps@users.noreply.github.com>
2023-12-20 12:13:44 +0100

    review formateur

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import "./Navbar.css";
 import { NavLink } from "react-router-dom";
+
 import mainLogoName from "../../assets/logo-ENN-name-hor.svg";
 import mainLogoName2 from "../../assets/logo-ENN-name-ver.svg";
 import profile from "../../assets/logo-profile.svg";
+
+import "./Navbar.css";
 
 function NavBar() {
   return (
