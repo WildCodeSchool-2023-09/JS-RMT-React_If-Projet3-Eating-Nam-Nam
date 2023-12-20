70c911a93795b2cdf36b8874a807bc5f0a18c640
Leslie2186 <leslie.moraud@gmail.com>
2023-12-20 11:22:06 +0100

    Routes GET PUT et DELETE opérationnelle pour les users

--- backend/src/router.js
+++ backend/src/router.js
@@ -8,9 +8,14 @@ const router = express.Router();
 
 // Import itemControllers module for handling item-related operations
 const itemControllers = require("./controllers/itemControllers");
+const userControllers = require("./controllers/userControllers");
 
-// Route to get a list of items
-router.get("/items", itemControllers.browse);
+// Route to get a list of users
+router.get("/users", userControllers.browse);
+// Route to put a user
+router.put("/users/:id", userControllers.update);
+// Route to delete a user
+router.delete("/users/:id", userControllers.deleteUser);
 
 // Route to get a specific item by ID
 router.get("/items/:id", itemControllers.read);
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
+import Admin from "./pages/admin/Admin";
+import Management from "./components/management/Management";
+import Dashboard from "./components/dashboard/Dashboard";
 import App from "./App";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/administration",
+    element: <Admin />,
+    children: [
+      {
+        path: "/administration",
+        element: <Dashboard />,
+      },
+      {
+        path: "/administration/management",
+        element: <Management />,
+      },
+    ],
   },
 ]);
 
