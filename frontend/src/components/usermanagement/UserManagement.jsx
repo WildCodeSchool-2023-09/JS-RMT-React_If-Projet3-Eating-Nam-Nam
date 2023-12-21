543c2964a53450c1362d8d3eabda9f355a466e0b
Leslie2186 <leslie.moraud@gmail.com>
2023-12-21 10:07:50 +0100

    Fix react toastify

--- frontend/src/components/usermanagement/UserManagement.jsx
+++ frontend/src/components/usermanagement/UserManagement.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
-import axios from "axios";
 import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 import "./UserManagement.css";
 
 function UserManagement() {
