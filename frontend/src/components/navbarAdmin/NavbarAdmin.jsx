bd7d58d76e8b4690026ae76b2012aa520e7876bd
Leslie2186 <leslie.moraud@gmail.com>
2023-12-21 09:59:00 +0100

    Modifications sur la navbarAdmin

--- frontend/src/components/navbarAdmin/NavbarAdmin.jsx
+++ frontend/src/components/navbarAdmin/NavbarAdmin.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./NavbarAdmin.css";
-import dashboard from "../../assets/boutonDashboard.png";
 import management from "../../assets/boutonManagement.png";
 
 function NavbarAdmin() {
   return (
-    <div className="containNavbar">
+    <div className="contain-navbar">
       <nav>
         <Link to="/administration/management">
           <img src={management} alt="Bouton Management" />
-        </Link>
-        <Link to="/administration">
-          <img src={dashboard} alt="Bouton Dashboard" />
         </Link>
       </nav>
     </div>
