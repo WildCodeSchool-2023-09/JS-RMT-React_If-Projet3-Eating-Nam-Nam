dc4ef694ffba6361bc873b6a9d17982c8b6b209c
Leslie2186 <leslie.moraud@gmail.com>
2023-12-22 12:34:32 +0100

    Route Post de la table Auth opérationnel

--- backend/src/router.js
+++ backend/src/router.js
@@ -11,6 +11,7 @@ const router = express.Router();
 const itemControllers = require("./controllers/itemControllers");
 */
 const userControllers = require("./controllers/userControllers");
+const authControllers = require("./controllers/authControllers");
 
 // Route to get a list of users
 router.get("/users", userControllers.browse);
@@ -18,6 +19,9 @@ router.get("/users", userControllers.browse);
 router.put("/users/:id", userControllers.update);
 // Route to delete a user
 router.delete("/users/:id", userControllers.destroy);
+
+// Route to post a new auth
+router.post("/auth", authControllers.add);
 
 /*
 // Route to get a specific item by ID
