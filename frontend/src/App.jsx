75d2d09846b84899f5a94943c142983f16e7f88a
Thibaud Dupuis <124290694+ThibaudDps@users.noreply.github.com>
2023-12-22 09:13:18 +0100

    resolve eslint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 
 import Navbar from "./components/navbar/Navbar";
-import NavbarMobile from "./components/navbarMobile/NavbarMobile";
+import NavbarMobile from "./components/NavbarMobile/NavbarMobile";
 import Footer from "./components/footer/Footer";
 
 import "../index.css";
