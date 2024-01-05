c9b07d2e0080594364b3e206b88c01b123876e4c
Valeriane SUDRE CHEVALIER <valeriane.chevalier@gmail.com>
2024-01-05 09:32:55 +0100

    correction des espaces

--- backend/src/router.js
+++ backend/src/router.js
@@ -6,30 +6,24 @@ const router = express.Router();
 /* ************************************************************************* */
 // Define Your API Routes Here
 /* ************************************************************************* */
-
 // Import itemControllers module for handling item-related operations
 /*
 const itemControllers = require("./controllers/itemControllers");
 */
 const userControllers = require("./controllers/userControllers");
 const recipeControllers = require("./controllers/recipeControllers");
-
 // Route to get a list of users
 router.get("/users", userControllers.browse);
 router.get("/recipes", recipeControllers.getRecipe);
-
 // Route to put a user
 router.put("/users/:id", userControllers.update);
 // Route to delete a user
 router.delete("/users/:id", userControllers.destroy);
-
 // Route to get a specific item by ID
 // router.get("/items/:id", itemControllers.read);
 router.get("/recipes/:id", recipeControllers.getRecipeByID);
-
 // Route to add a new item
 // router.post("/items", itemControllers.add);
-
 /* ************************************************************************* */
 
 module.exports = router;
