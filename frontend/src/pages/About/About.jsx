00952c21d62140c9b0cf98d7afd1092ff304dbe9
Thibaud Dupuis <124290694+ThibaudDps@users.noreply.github.com>
2024-01-10 10:11:01 +0100

    Feature exceptionnelle + lien github

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import logo from "../../assets/logo-ENN.svg";
 import val from "../../assets/val.png";
@@ -16,20 +17,28 @@ function About() {
           <h2 className="about-h2">Our Team</h2>
         </div>
         <div className="about-fish-val">
-          <img className="img-fish" src={val} alt="val" />
-          <h4 className="about-name-val">Valériane</h4>
+          <Link to="https://github.com/valihna">
+            <img className="img-fish" src={val} alt="val" />{" "}
+            <h4 className="about-name-val">Valériane</h4>
+          </Link>
         </div>
         <div className="about-fish-leslie">
-          <img className="img-fish" src={leslie} alt="leslie" />
-          <h4 className="about-name-leslie">Leslie</h4>
+          <Link to="https://github.com/Leslie2186">
+            <img className="img-fish" src={leslie} alt="leslie" />
+            <h4 className="about-name-leslie">Leslie</h4>
+          </Link>
         </div>
         <div className="about-fish-thib">
-          <img className="img-fish" src={thib} alt="thib" />
-          <h4 className="about-name-thib">Thibaud</h4>
+          <Link to="https://github.com/ThibaudDps">
+            <img className="img-fish" src={thib} alt="thib" />
+            <h4 className="about-name-thib">Thibaud</h4>
+          </Link>
         </div>
         <div className="about-fish-greg">
-          <img className="img-fish" src={greg} alt="greg" />
-          <h4 className="about-name-greg">Grégory</h4>
+          <Link to="https://github.com/GreG6431">
+            <img className="img-fish" src={greg} alt="greg" />{" "}
+            <h4 className="about-name-greg">Grégory</h4>
+          </Link>
         </div>
       </div>
     </div>
