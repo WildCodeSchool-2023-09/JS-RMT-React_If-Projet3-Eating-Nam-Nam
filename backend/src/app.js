4ed496f84f46fafacbead5d4a391adddfbeae14f
Thibaud Dupuis <124290694+ThibaudDps@users.noreply.github.com>
2024-01-12 15:08:34 +0100

    Connexion user + role + admin + navbar role + hotfix css

--- backend/src/app.js
+++ backend/src/app.js
@@ -31,7 +31,6 @@ app.use(
     origin: [
       process.env.FRONTEND_URL, // keep this one, after checking the value in `backend/.env`
     ],
-    credentials: true,
   })
 );
 
--- frontend/src/services/connexion.js
+++ frontend/src/services/connexion.js
@@ -2,8 +2,6 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
-
-  withCredentials: true,
 });
 
 export default instance;
