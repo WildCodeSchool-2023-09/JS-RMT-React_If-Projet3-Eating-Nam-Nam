4b1f6b470565551751fa8bb7ca90bd78b2486d3c
Leslie <125278372+Leslie2186@users.noreply.github.com>
2024-01-16 12:25:53 +0100

    Update Navbar.jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,13 +2,14 @@ import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { AuthContext } from "../../contexts/Auth";
 import profile from "../../assets/logo-profile.svg";
+import mainLogoName from "../../assets/logo-ENN-name-hor.svg";
+import mainLogoName2 from "../../assets/logo-ENN-name-ver.svg";
 
 import "./Navbar.css";
 
 function NavBar() {
   const { connected } = useContext(AuthContext);
   const { infosUser } = useContext(AuthContext);
- 
   return (
     <div>
       <nav className="nav">
