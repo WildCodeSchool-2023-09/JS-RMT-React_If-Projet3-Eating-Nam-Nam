7b907ecf69cde1243fa9239ee6d8677d6cdfa190
Thibaud Dupuis <124290694+ThibaudDps@users.noreply.github.com>
2024-01-22 16:30:25 +0100

    profile css + responsive + en attente implantation data

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -14,21 +14,32 @@ function Profile() {
           />
           <h2 className="profile-h2">Name</h2>
         </div>
+        <div className="profile-birthday">
+          <h3>Birthday</h3>
+          <p>birth date</p>
+        </div>
+        <div className="profile-regime">
+          <h3>Regime</h3>
+          <p>Regime name</p>
+        </div>
       </div>
       <div className="profile-page-sub">
         <div className="profile-frame-friends">
           <div className="profile-title">
             <h2 className="profile-h2">Friends</h2>
+            <p>Place friends here</p>
           </div>
         </div>
         <div className="profile-frame-recipes">
           <div className="profile-title">
             <h2 className="profile-h2">Recipes</h2>
+            <p>Place recipes here</p>
           </div>
         </div>
         <div className="profile-frame-favorites">
           <div className="profile-title">
             <h2 className="profile-h2">Favorites</h2>
+            <p>Place favorites here</p>
           </div>
         </div>
       </div>
