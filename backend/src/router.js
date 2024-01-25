e1e1623ebf499e6c30ef9b7672102d219fb92bec
Leslie2186 <leslie.moraud@gmail.com>
2024-01-25 14:42:54 +0100

    Correctif MVC table ingrédients

--- backend/src/router.js
+++ backend/src/router.js
@@ -29,8 +29,6 @@ router.get("/favorites", checkCredentials, favoriteControllers.browse);
 router.get("/regime", checkCredentials, regimeControllers.browse);
 router.get("/recipes/:id", checkCredentials, recipeControllers.read);
 router.get("/favorites/:id", checkCredentials, favoriteControllers.read);
-router.get("/ingredients", checkCredentials, ingredientControllers.browse);
-router.post("/ingredients", checkCredentials, ingredientControllers.add);
 
 // Route to put a user
 router.put("/users/:id", checkCredentials, userControllers.update);
@@ -38,6 +36,7 @@ router.put("/users/:id", checkCredentials, userControllers.update);
 router.delete("/users/:id", checkCredentials, userControllers.destroy);
 // Route to post a new auth
 
+router.post("/ingredients", checkCredentials, ingredientControllers.add);
 router.post("/auth", checkCredentials, authControllers.add);
 router.post("/users", checkCredentials, userControllers.add);
 router.post("/favorites", checkCredentials, favoriteControllers.add);
