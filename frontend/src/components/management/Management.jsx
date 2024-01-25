6f70107ffaf26829420cd81ed1785d784ee936d4
Leslie2186 <leslie.moraud@gmail.com>
2024-01-25 15:52:30 +0100

    Dashboard recipes front ok

--- frontend/src/components/management/Management.jsx
+++ frontend/src/components/management/Management.jsx
@@ -1,4 +1,5 @@
 import UserManagement from "../usermanagement/UserManagement";
+import RecipeManagement from "../recipemanagement/RecipeManagement";
 import "./Management.css";
 
 function AdminUsers() {
@@ -7,6 +8,9 @@ function AdminUsers() {
       <h2>Dashboard Admin</h2>
       <div className="user-management">
         <UserManagement />
+      </div>
+      <div className="recipe-management">
+        <RecipeManagement />
       </div>
     </div>
   );
