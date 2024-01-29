3a6c2c457d9a6c1c710c7f44be77ec1d55714398
Leslie2186 <leslie.moraud@gmail.com>
2024-01-29 16:55:06 +0100

    Filtres opérationnels sur les recettes et  modification de la data pour retirer les sdfsffs et autres dfsffgfdg

--- frontend/src/components/recipes/RecipeCard.jsx
+++ frontend/src/components/recipes/RecipeCard.jsx
@@ -1,14 +1,60 @@
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useState } from "react";
 import RecipeList from "./RecipeList";
 import "./Recipes.css";
 
 function RecipeCard({ recipes }) {
+  const sections = new Set(recipes.map((recipe) => recipe.section));
+  const difficulties = new Set(recipes.map((recipe) => recipe.difficulty));
+  const diets = new Set(recipes.map((recipe) => recipe.diet));
+
+  const [sectionFilter, setSectionFilter] = useState("all");
+  const [difficultiesFilter, setDifficultiesFilter] = useState("all");
+  const [dietsFilter, setDietsFilter] = useState("all");
+
   return (
-    <div className="frame-container">
-      {recipes.map((recip) => (
-        <RecipeList key={recip.id} recip={recip} />
-      ))}
+    <div>
+      <form className="section-recipe">
+        <select onChange={(e) => setSectionFilter(e.target.value)}>
+          <option value="all">All sections</option>
+          {Array.from(sections).map((section) => (
+            <option key={section} value={section}>
+              {section}
+            </option>
+          ))}
+        </select>
+
+        <select onChange={(e) => setDifficultiesFilter(e.target.value)}>
+          <option value="all">All difficulties</option>
+          {Array.from(difficulties).map((difficulty) => (
+            <option key={difficulty} value={difficulty}>
+              {difficulty}
+            </option>
+          ))}
+        </select>
+
+        <select onChange={(e) => setDietsFilter(e.target.value)}>
+          <option value="all">All diet</option>
+          {Array.from(diets).map((diet) => (
+            <option key={diet} value={diet}>
+              {diet}
+            </option>
+          ))}
+        </select>
+      </form>
+      <div className="frame-container">
+        {recipes
+          .filter(
+            (recipe) =>
+              (recipe.section === sectionFilter || sectionFilter === "all") &&
+              (recipe.difficulty === difficultiesFilter ||
+                difficultiesFilter === "all") &&
+              (recipe.diet === dietsFilter || dietsFilter === "all")
+          )
+          .map((recip) => (
+            <RecipeList key={recip.id} recip={recip} />
+          ))}
+      </div>
     </div>
   );
 }
